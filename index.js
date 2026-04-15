d58d7d4e66687027178710cf79cad1b7cee69f8a
Daniel Flores <danieel.zen@gmail.com>
2026-04-15 01:09:21 -0600

    feat: HTTP request parser and basic routing

--- index.js
+++ index.js
@@ -8,11 +8,26 @@ const server = net.createServer((socket) => {
 
         console.log('RAW REQUEST:\n' + data.toString())
 
-        socket.write('HTTP/1.1 200 OK\r\n' +
-                    'Content-Type: text/plain\r\n' +
-                    'Content-Length: 13\r\n' +
-                    '\r\n' +
-                    'Hello, World!')
+        const parsed = parse(data.toString())
+        console.log(parsed)
+
+        let x = 'Not found'
+        let y = '200 OK'
+
+        if (parsed.path == '/') {
+            x = 'Hello, World!'
+        } else if (parsed.path == '/about') {
+            x = 'About page'
+        } else {
+            y = '404 Not Found'
+        }
+
+       socket.write('HTTP/1.1 ' + y + '\r\n' +
+            'Content-Type: text/plain\r\n' +
+            'Content-Length:' + x.length + '\r\n' +
+            '\r\n' +
+            x)
+      
     })
 
     socket.on('close', () => {
@@ -27,3 +42,11 @@ const server = net.createServer((socket) => {
 server.listen(3000, () => {
     console.log('Servidor escuchando en puerto 3000')
 })
+
+function parse(request) {
+    const arr = request.split('\r\n')
+    const a = arr[0]
+    const b = a.split(' ')
+
+    return {method: b[0], path: b[1]}
+}
